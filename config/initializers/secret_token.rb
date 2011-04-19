002da60763dfc2f98753445e0679c8473d45615b
Josh Sullivan <josh@dipperstove.com>
2011-04-19 00:02:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Henry::Application.config.secret_token = 'a85ef78c584f59efc0b00e31056f0e93cdf4b1756ae592afee6b2d0f70cad03fe12510744cb3074209ad03a605f697e6df61cd9cbfe37b947e4db48737188881'
